5b2ec0f8c15dde4e77aca4f1e4c39bab98dd6c1d
Adrian <103188006+Adrian-Cxll@users.noreply.github.com>
2023-01-29 17:27:18 +0100

    Buy fix Website added

---
+++ websites-bin/Game-Presentation/main.js
@@ -0,0 +1,33 @@
+AOS.init();
+
+
+
+// let element = document.querySelectorAll(".section-three-fade");
+// function fadeOut() {
+//     var distInView = element.getBoundingClientRect().top - window.innerHeight + 20;
+//     if (distInView < 0) {
+//         element.classList.add("inView");
+//     } else {
+//         element.classList.remove("inView");
+//     }
+// }
+// fadeOut();
+
+
+// let elem = document.querySelectorAll(".tile");
+// // console.log(elementsArray);
+// window.addEventListener('scroll', fadeIn ); 
+// function fadeIn() {
+//         var distInView = elem.getBoundingClientRect().top - window.innerHeight + 20;
+//         if (distInView < 0) {
+//             elem.classList.add("inView");
+//         } else {
+//             elem.classList.remove("inView");
+//         }
+//     }
+// fadeIn();
+
+// idea ->
+// wenn man auf die seite des charakers kommt
+// steht mit dickem Font der Name des main charakers?
+// wenn du kp 3-5sek auf der seite warst, faded (leicht) und wischt der name nach rechts!
